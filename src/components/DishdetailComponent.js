f0413affba710ae8b25d8131bd2780abc4e4f658
RajPatel0111 <rajpatel031798@gmail.com>
2020-07-13 20:52:44 -0400

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -14,22 +14,17 @@ class Dishdetail extends Component {
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
                     &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </li>
             )
         })
         return (
-            <div className='col-12 col-md-5 m-1'>
+            <div>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
                     {coment}
                 </ul>
-
             </div>
         )
     }
@@ -37,7 +32,7 @@ class Dishdetail extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <div className='col-12 col-md-5 m-1'>
+                <div>
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
@@ -61,10 +56,17 @@ class Dishdetail extends Component {
         const dishItem = this.renderDish(dish)
         const commentItem = this.renderComments(dish.comments)
         return (
-            <div className='row'>
-                {dishItem}
-                {commentItem}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {dishItem}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {commentItem}  
+                    </div>            
+                </div>
             </div>
+
         )
     }
 }
