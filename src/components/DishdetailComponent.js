7158eea2014a787bf761448c0d61ffd365ff839a
RajPatel0111 <rajpatel031798@gmail.com>
2020-08-23 19:25:31 -0400

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ export class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     } 
 
     render() {
@@ -132,7 +132,7 @@ export class CommentForm extends Component {
         );
     }
 }
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, dishId}) {
         if (comments != null) 
             return (
                 <div className="col-12 col-md-5 m-1">
@@ -148,7 +148,7 @@ export class CommentForm extends Component {
                             </li>
                         );
                     })}
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                     </ul>
                     </div>
             );
@@ -173,7 +173,9 @@ export class CommentForm extends Component {
                     </div>
                     <div className="row">
                         <RenderDish dish = {props.dish}/>
-                        <RenderComments comments = {props.comments}/>
+                        <RenderComments comments = {props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
                     </div>
                 </div>
             );}
